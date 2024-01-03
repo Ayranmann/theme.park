02556f8b5953ad316efab20e079757b21d5496c3
Ayranmann <Ayranmann@users.noreply.github.com>
2024-01-03 21:28:04 +0000

    deploy: b159f8ea43d5ab5454d476e93fb21fa97ab8f76e

---
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -0,0 +1,22 @@
+
+ //Custom Text Header //
+ // ACSII slant font
+
+// Needs to be inside <section id="login" class="shadow">
+// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
+
+let custom_text_header = `
+<div class="custom-text-header"> <pre>                     __                             ___       __  
+
+ .d8888b.  888b     d888 888     888 Y88b   d88P 
+d88P  Y88b 8888b   d8888 888     888  Y88b d88P  
+Y88b.      88888b.d88888 888     888   Y88o88P   
+ "Y888b.   888Y88888P888 888     888    Y888P    
+    "Y88b. 888 Y888P 888 888     888    d888b    
+      "888 888  Y8P  888 888     888   d88888b   
+Y88b  d88P 888   "   888 Y88b. .d88P  d88P Y88b  
+ "Y8888P"  888       888  "Y88888P"  d88P   Y88b 
+                                                 
+                                                                 </pre> </div>
+`;
+document.getElementById("login").innerHTML += custom_text_header
