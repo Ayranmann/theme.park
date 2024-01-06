4fea654407ac48fd3a1bfa9e08093f5f9b7c6208
Ayranmann <Ayranmann@users.noreply.github.com>
2024-01-06 17:37:28 +0000

    deploy: 23e2ef5005597ae510cf2cb9e39c95367f1470a2

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -13,6 +13,8 @@ let custom_text_header = `
  |  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` |   / / 
  | |  | (_| \__ \__ \\ V  V / (_) | | | (_| |  |_|  
  |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|  (_)   
+                                        
+                                         
                                          </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
